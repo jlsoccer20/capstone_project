84a4ee4e9a8858d8e5c204c3b8f08c8b2ddd6ebf
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-10-15 01:17:50 +0200

    adding images

--- genshin_react/src/components/Header.js
+++ genshin_react/src/components/Header.js
@@ -9,30 +9,28 @@ export default function Header(){
         <h2>Jessica Link</h2>
         <img className="header-image" src={process.env.PUBLIC_URL + "/GenshinImpact.jpg"} alt="Genshin Banner"></img>
         
-        <img className="header-image" src={process.env.PUBLIC_URL + "/faruzan.png"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/hutao.png"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/mona.webp"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/noelle.png"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/shenhe.png"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/sucrose.png"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/wanderer.png"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/xiao.png"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/rosaria.png"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/faruzan.png"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/hutao.png"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/mona.webp"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/noelle.png"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/shenhe.png"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/sucrose.png"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/wanderer.png"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/xiao.png"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/rosaria.png"}  alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/ganyu.png"}  alt="Genshin Banner"></img>
 
+        <img className="character" src={process.env.PUBLIC_URL + "/ayaka.jpg"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/childe.jpg"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/diona.jpg"} alt="Genshin Banner"></img>
+        <img className="character" src={process.env.PUBLIC_URL + "/eula.jpg"} alt="Genshin Banner"></img>
+        
+        <img className="character" src={process.env.PUBLIC_URL + "/qiqi.jpg"} alt="Genshin Banner"></img>
 
-        <img className="header-image" src={process.env.PUBLIC_URL + "/ayaka.jpg"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/childe.jpg"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/diona.jpg"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/eula.jpg"} alt="Genshin Banner"></img>
-        <img className="header-image" src={process.env.PUBLIC_URL + "/ganyu.jpg"} alt="Genshin Banner"></img>
-
-        <img className="header-image" src={process.env.PUBLIC_URL + "/qiqi.jpg"} alt="Genshin Banner"></img>
 
         <img className="header-image" src={process.env.PUBLIC_URL + "/GenshinIcon.png"} alt="Genshin Banner"></img>
         
-        
 
-        
         <p></p>
         <img className="icon" src={process.env.PUBLIC_URL + "/primogemIcon.png"} alt="Genshin Banner"></img>
         <img className="icon" src={process.env.PUBLIC_URL + "/GenshinIcon.png"} alt="Genshin Banner"></img>
