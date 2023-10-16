ee6aa5273f3eef15f6e4b25528d73c9bc2db9e50
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-10-16 21:10:29 +0200

    Connecting to the API

--- genshin_react/src/App.js
+++ genshin_react/src/App.js
@@ -1,22 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import About from './components/About';
 import Header from './components/Header';
 
+
+
 function App() {
-    return (
-        <Router>
-            <Header />
-            
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                
-            </Routes>
-        </Router>
-    );
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('https://gsi.fly.dev/');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <Router>
+        <Header />  
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />    
+      </Routes>
+
+    <div className="App">
+      <h1>API Data:</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        //<pre>{JSON.stringify(data, null, 2)}</pre>
+        <div>
+            <h2>Character List:</h2>
+            {data && data.characters && data.characters.length > 0 ? (
+            <ul>
+              {data.characters.map((character, index) => (
+                <li key={index}>
+                  <strong>Name:</strong> {character.name}, <strong>Weapon Type:</strong> {character.weaponType}
+                </li>
+              ))}
+            </ul>
+            ) : (
+                <p>No character data available.</p>
+                )}
+        </div>
+        )}
+    </div>
+    </Router>
+  );
 }
 
 export default App;
--- genshin_react/src/components/About.js
+++ genshin_react/src/components/About.js
@@ -3,12 +3,14 @@
 export default function About() {
     return (
         <div>
-            <h1>About React Pets</h1>
+            <h1>About Genshin Wish Simulator</h1>
             <p>
-                Welcome to React Pets, where we strive to provide a loving and
-                caring environment for animals in need, offering them a second
-                chance at a forever home.
+                Welcome to the Genshin Impact Wish Simulator, 
+                where your favorite Genshin characters are just a click away 
+                (and a bit of luck)!
+
             </p>
+            <p>This project implements many topics covered in the Dev10 Academy 2023 cohort. </p>
         </div>
     );
 }
