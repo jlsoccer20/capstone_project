31eae76b49a3ddcc9dbb13925b72ad3a56fdf261
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-10-21 15:53:48 +0200

    Added Paimon logo for website tab icon

--- genshin_react/src/components/Header.js
+++ genshin_react/src/components/Header.js
@@ -4,6 +4,7 @@ import './styles.css';
 export default function Header(){
     
     return <header className="header">
+        
         <h1 className="header-title"><span>Genshin Wish Simulator</span></h1>
         </header>;
 }
--- genshin_react/src/components/Home.js
+++ genshin_react/src/components/Home.js
@@ -3,7 +3,8 @@ function Home() {
     <div>
 
         <img className="header-image-home" src={process.env.PUBLIC_URL + "/GenshinImpact.jpg"} alt="Genshin Banner"></img> 
-    
+        <link rel="icon" href="favicon.ico" type="image/x-icon"></link>
+
         <h1>Welcome!</h1>
         <p> Please use the navigation links at the top.</p>
 
