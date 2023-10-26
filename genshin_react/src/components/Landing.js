554879fffaad47ed521df627f9de19d97a57e9ff
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-10-27 00:30:47 +0200

    Readded Landing page

---
+++ genshin_react/src/components/Landing.js
@@ -0,0 +1,11 @@
+function Landing() {
+    return (
+      <img
+        className="header-image-home"
+        src={process.env.PUBLIC_URL + "/genshinbackground.jpg_large"}
+        alt="Genshin Banner"
+      ></img>
+    );
+  }
+  
+  export default Landing;
